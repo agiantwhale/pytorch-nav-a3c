0ab1436b7bd23341fde3783d91502087271686ee
Il Jae Lee <agiantwhale@gmail.com>
2018-05-06 19:39:05 -0500

    Added param gen for PBS config generation

---
+++ paramgen.py
@@ -0,0 +1,52 @@
+from __future__ import print_function
+
+import argparse
+import os
+import numpy as np
+
+parser = argparse.ArgumentParser()
+parser.add_argument('root_path')
+parser.add_argument('config_name')
+parser.add_argument('email')
+
+
+def main(args):
+    root_base = os.path.abspath(args.root_path)
+    hyperparams = dict(lr=np.random.uniform(np.power(10., -4), 5 * np.power(10., -4)),
+                       entropy_coef=np.random.uniform(np.power(10., -4), np.power(10., -3)),
+                       num_steps=50,
+                       conv_depth_loss_coef=np.random.choice([1 / 3.0, 10, 33]),
+                       lstm_depth_loss_coef=np.random.choice([1, 10 / 3.0, 10]),
+                       save_interval=1000,
+                       log_interval=10,
+                       num_processes=16,
+                       num_torch_threads=16,
+                       checkpoint_path=os.path.join(root_base, 'checkpoint', args.config_name),
+                       video_path=os.path.join(root_base, 'media', args.config_name))
+
+    headers = """
+    #PBS -N {}
+    #PBS -j oe
+    #PBS -l walltime=60:00:00
+    #PBS -l nodes=1:ppn=1:gpus=1
+    #PBS -S /bin/bash
+    #PBS -m abe
+    #PBS -M {}
+    #PBS -V
+    """.format(args.config_name, args.email)
+
+    for l in headers.splitlines():
+        r = l.strip()
+
+        if r:
+            print(r)
+
+    print()
+    print("python main.py \\")
+    for idx, (flag, value) in enumerate(hyperparams.items()):
+        print(" " * 10 + "{:<50} {}".format('--{}={}'.format(flag.replace("_", "-"), value),
+                                            '\\' if idx + 1 < len(hyperparams) else str()))
+
+
+if __name__ == "__main__":
+    main(parser.parse_args())
